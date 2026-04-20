82042d09d47588026abe193fd96ea52bee086743
Sanemi-sama <hansayah7@gmail.com>
2026-04-20 12:17:11 +0100

    Ajout d'un script de sécurité

---
+++ script_sec.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Commande corrigée (sans le -t)
+docker exec -i -e OPENSEARCH_JAVA_HOME=/usr/share/wazuh-indexer/jdk sgd_wazuh_indexer \
+  /usr/share/wazuh-indexer/plugins/opensearch-security/tools/securityadmin.sh \
+  -cd /usr/share/wazuh-indexer/config/opensearch-security/ \
+  -icl -nhnv \
+  -cacert /usr/share/wazuh-indexer/config/certs/root-ca.pem \
+  -cert /usr/share/wazuh-indexer/config/certs/admin.pem \
+  -key /usr/share/wazuh-indexer/config/certs/admin-key.pem \
+  -h localhost

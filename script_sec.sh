2a93604ef10b2db56006b0ae7d7a768ee2697089
Sanemi-sama <hansayah7@gmail.com>
2026-04-20 12:22:27 +0100

    Modification du script de sécurité

--- script_sec.sh
+++ script_sec.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-
-# Commande corrigée (sans le -t)
 docker exec -i -e OPENSEARCH_JAVA_HOME=/usr/share/wazuh-indexer/jdk sgd_wazuh_indexer \
   /usr/share/wazuh-indexer/plugins/opensearch-security/tools/securityadmin.sh \
   -cd /usr/share/wazuh-indexer/config/opensearch-security/ \
